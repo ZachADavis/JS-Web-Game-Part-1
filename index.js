c6494b1a3fbdcacd331c3265014634dc3a89fb55
Zachary Davis <kicksilver@icloud.com>
2022-05-24 19:32:30 -0400

    Homework

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(char, s, x, y, z){
+char = document.createElement('img')
+char.src = s
+char.style.position = z
+char.style.left = x
+char.style.bottom = y
+document.body.append(char)
+}
+
+function newItem(item, s, x, y, z){
+    item = document.createElement('img')
+    item.src = s
+    item.style.position = z
+    item.style.left = x
+    item.style.bottom = y
+    document.body.append(item)
+    item.addEventListener('dblclick', function(){
+        item.remove()
+})
+}
+
+newImage('greenCharacter', ' assets/green-character.gif', '100px', '100px', 'fixed')
+newImage('pineTree', ' assets/pine-tree.png', '450px', '200px', 'fixed')
+newImage('tree', ' assets/tree.png', '200px', '300px', 'fixed')
+newImage('pillar', ' assets/pillar.png', '350px', '100px', 'fixed')
+newImage('crate', ' assets/crate.png', '150px', '200px', 'fixed')
+newImage('well', ' assets/well.png', '500px', '425px', 'fixed')
+
+
+newItem('sword', ' assets/sword.png', '500px', '405px', 'fixed')
+newItem('shield', ' assets/shield.png', '165px', '185px', 'fixed')
+newItem('staff', ' assets/staff.png', '600px', '100px', 'fixed')
